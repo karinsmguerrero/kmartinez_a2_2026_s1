4597e11ef9b1baac185358753df540f34e7ecd1a
Karina <Karinamguerrero@estudiantec.cr>
2026-03-15 15:35:04 -0600

    [feature] add selection for model and text

--- Models/Serial.cpp
+++ Models/Serial.cpp
@@ -4,37 +4,6 @@
 #include "../FileManagement/FileReader.h"
 #include "../Mapper/Mapper.h"
 
-std::vector<std::string> getTopKWords(const std::unordered_map<std::string, int> &wordCounts, size_t k)
-{
-    // Create a vector of pairs from the unordered_map
-    std::vector<std::pair<std::string, int>> wordCountVector(wordCounts.begin(), wordCounts.end());
-
-    // Sort the vector based on the counts in descending order
-    std::sort(wordCountVector.begin(), wordCountVector.end(),
-              [](const std::pair<std::string, int> &a, const std::pair<std::string, int> &b)
-              {
-                  return b.second < a.second; // Sort in descending order
-              });
-
-    // Extract the top k words
-    std::vector<std::string> topKWords;
-    for (size_t i = 0; i < std::min(k, wordCountVector.size()); ++i)
-    {
-        topKWords.push_back(wordCountVector[i].first);
-    }
-
-    return topKWords;
-}
-
-void printWordCounts(std::vector<std::string> topKWords)
-{
-    // Function to print the word counts
-    for (const auto &word : topKWords)
-    {
-        std::cout << word << ": " << localHashMap[word] << std::endl;
-    }
-}
-
 int runMapReduce(std::string filePath)
 {
     // Read file and get lines
@@ -43,10 +12,11 @@ int runMapReduce(std::string filePath)
     if (!lines.empty())
     {
         std::cout << "File read successfully. Number of lines: " << getLineCount(lines) << std::endl;
-
-        processTextChunk(lines);                                             // Process the chunk of text
-        std::vector<std::string> topkwords = getTopKWords(localHashMap, 10); // Get the top 10 most common words
-        printWordCounts(topkwords);                                          // Print the top 10 most common words
+        // Map phase: Process the chunk of text and count word occurrences
+        processTextChunk(lines);    
+        // Reduce phase: Add the word counts
+        // No Reduce phase needed for the serial model since we are directly updating the localHashMap in the Mapper
+        // and there are no other threads to combine results with.
     }
     else
     {
--- main.cpp
+++ main.cpp
@@ -3,6 +3,37 @@
 #include <algorithm>
 #include "Models/Serial.cpp"
 
+std::vector<std::string> getTopKWords(const std::unordered_map<std::string, int> &wordCounts, size_t k)
+{
+    // Create a vector of pairs from the unordered_map
+    std::vector<std::pair<std::string, int>> wordCountVector(wordCounts.begin(), wordCounts.end());
+
+    // Sort the vector based on the counts in descending order
+    std::sort(wordCountVector.begin(), wordCountVector.end(),
+              [](const std::pair<std::string, int> &a, const std::pair<std::string, int> &b)
+              {
+                  return b.second < a.second; // Sort in descending order
+              });
+
+    // Extract the top k words
+    std::vector<std::string> topKWords;
+    for (size_t i = 0; i < std::min(k, wordCountVector.size()); ++i)
+    {
+        topKWords.push_back(wordCountVector[i].first);
+    }
+
+    return topKWords;
+}
+
+void printTopKWordCounts(std::vector<std::string> topKWords, const std::unordered_map<std::string, int> &hashMap)
+{
+    // Function to print the word counts
+    for (const auto &word : topKWords)
+    {
+        std::cout << word << ": " << hashMap.at(word) << std::endl;
+    }
+}
+
 int main()
 {
     // List of files to process
@@ -11,7 +42,36 @@ int main()
         "Assets/fellowship.txt",
         "Assets/In Search of Lost Time.txt"};
 
-    runMapReduce(files[2]); // Process the first file for testing
+    std::cout << "Select a model to run:" << std::endl;
+    std::cout << "1. Serial Model" << std::endl;
+    std::cout << "2. Parallel Model" << std::endl;
+    std::cout << "Enter your choice (1 or 2): ";
+    int modelChoice;
+    std::cin >> modelChoice;
+
+    std::cout << "Select text to process: " << std::endl;
+    std::cout << "1. El Conde de Monte Cristo" << std::endl;
+    std::cout << "2. The Fellowship of the Ring" << std::endl;
+    std::cout << "3. In Search of Lost Time" << std::endl;
+    std::cout << "Enter your choice (1, 2, or 3): ";
+    int fileChoice;
+    std::cin >> fileChoice;
+
+    if (modelChoice == 1)
+    {
+        std::cout << "------------ Running Serial Model on " << files[fileChoice - 1] << "------------ "<< std::endl;
+        runMapReduce(files[fileChoice - 1]); // Process the selected file
+        printTopKWordCounts(getTopKWords(localHashMap, 10), localHashMap); // Print the top 10 most common words
+    }
+    else if (modelChoice == 2)
+    {
+        std::cout << "------------ Running Parallel Model on " << files[fileChoice - 1] << "------------ "<< std::endl;
+    }
+    else
+    {
+        std::cout << "Invalid choice. Exiting." << std::endl;
+        return 1;
+    }
 
     return 0;
 }
